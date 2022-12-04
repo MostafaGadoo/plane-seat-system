794112af87eb04b8027d22549b14309dc1c19c0a
MostafaGadoo <73221261+MostafaGadoo@users.noreply.github.com>
2022-12-04 17:29:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-// const joi = require('joi');
+
 
 const initiateDBConnection = require('./config/db');
 const ticketRouter = require('./routers/ticketRoute');
